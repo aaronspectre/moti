1e1509d8f2c243dd0636ebc0e2a7cceb0204b87b
Harry Potter <potter@example.com>
2022-10-06 09:12:07 +0500

    All

--- main/post.py
+++ main/post.py
@@ -6,69 +6,64 @@ conn = http.client.HTTPConnection("drogrammer.uz", None)
 dataList = []
 boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
 dataList.append(encode('--' + boundary))
-dataList.append(encode('Content-Disposition: form-data; name=categoryCreateRequest;'))
+dataList.append(encode('Content-Disposition: form-data; name=request;'))
 
 dataList.append(encode('Content-Type: {}'.format('application/json')))
 dataList.append(encode(''))
 
 dataList.append(encode("""{
-    \"categoryName\": {
-      \"uz\": {
-        \"lang\": \"uz\",
-        \"text\": \"uzbek sub\"
-      },
-      \"ru\": {
-        \"lang\": \"ru\",
-        \"text\": \"ruscha sub\"
-      },
-      \"eng\": {
-        \"lang\": \"eng\",
-        \"text\": \"ingliz  sub\"
-      }
+  \"categoryId\": 7,
+  \"name\": {
+    \"uz\": {
+      \"lang\": \"uz\",
+      \"text\": \"Losos balig'lik Maki\"
+    },
+    \"ru\": {
+      \"lang\": \"ru\",
+      \"text\": \"Маки с лососем \"
+    },
+    \"eng\": {
+      \"lang\": \"eng\",
+      \"text\": \"Maki with salmon\"
     }
-  }"""))
+  },
+  \"discount\": 0.1,
+  \"description\": {
+    \"uz\": {
+      \"lang\": \"uz\",
+      \"text\": \"Salmon Maki Rolls sushining eng taniqli shaklidir. Ular xom yangi losos va piyoz bilan to'ldirilgan va nori (dengiz o'tlari) bilan o'ralgan sushi guruchining klassik rulonlari\"
+    },
+    \"ru\": {
+      \"lang\": \"ru\",
+      \"text\": \"Роллы с лососем маки — самая узнаваемая форма суши. Это классические роллы из риса для суши, фаршированные сырым свежим лососем и зеленым луком и завернутые в нори (морские водоросли).\"
+    },
+    \"eng\": {
+      \"lang\": \"eng\",
+      \"text\": \"Salmon Maki Rolls are the most recognisable form of Sushi. They're the classic rolls of sushi rice stuffed with raw fresh salmon and spring onion and wrapped in nori (seaweed). \"
+    }
+  },
+  \"price\": 39000,
+  \"readyTime\": \"45 min\"
+}"""))
+dataList.append(encode('--' + boundary))
+dataList.append(encode('Content-Disposition: form-data; name=image; filename={0}'.format('Boys Cartoon Image.jpg')))
+
+fileType = mimetypes.guess_type('C:/Users/localhost/Downloads/pic.jpg')[0] or 'application/octet-stream'
+dataList.append(encode('Content-Type: {}'.format(fileType)))
+dataList.append(encode(''))
+
+with open('C:/Users/localhost/Downloads/pic.jpg', 'rb') as f:
+  dataList.append(f.read())
 dataList.append(encode('--'+boundary+'--'))
 dataList.append(encode(''))
 body = b'\r\n'.join(dataList)
 payload = body
-print(payload)
 headers = {
-   'Content-type': 'multipart/form-data; boundary={}'.format(boundary) ,
-   'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5OTg5NzYzMzE4MDkiLCJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9VU0VSIn0seyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpYXQiOjE2NjQwOTc2NTcsImV4cCI6Mzc2NjQwOTc2NTd9.ORHHPOHwcEerLfi4_DGhPvZOwWTyNDvcXuxjR01OLyrlOl_lJldx97VY5UjZ1ntiR9A4Hi8r9J8PBvEKtpkUgg'
+   'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5OTg5NzYzMzE4MDkiLCJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9VU0VSIn0seyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpYXQiOjE2NjQwOTc2NTcsImV4cCI6Mzc2NjQwOTc2NTd9.ORHHPOHwcEerLfi4_DGhPvZOwWTyNDvcXuxjR01OLyrlOl_lJldx97VY5UjZ1ntiR9A4Hi8r9J8PBvEKtpkUgg',
+   'Content-type': 'multipart/form-data; boundary={}'.format(boundary) 
 }
-# conn.request("POST", "/api/v1//category", payload, headers)
+# conn.request("POST", "/api/v1/product", payload, headers)
 # res = conn.getresponse()
 # data = res.read()
-# print(data.decode("utf-8"))
-
-
-import requests
-
-boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
-data = {
-	"categoryName": {
-		"uz": {
-			"lang": "uz",
-			"text": "uzbek sub"
-		},
-		"ru": {
-			"lang": "ru",
-			"text": "ruscha sub"
-		},
-		"eng": {
-			"lang": "eng",
-			"text": "ingliz  sub"
-		}
-	}
-}
-data = '{\n    "categoryName": {\n      "uz": {\n        "lang": "uz",\n        "text": "uzbek sub"\n},\n      "ru": {\n        "lang": "ru",\n        "text": "ruscha sub"\n      },\n      "eng": {\n        "lang": "eng",\n        "text": "ingliz  sub"\n      }\n    }\n  }'
-data = f"""--{boundary}\r\nContent-Disposition: form-data; name=categoryCreateRequest;\r\nContent-Type: application/json\r\n\r\n{data}\r\n--{boundary}--\r\n""".encode()
-headers = {
-	'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5OTg5NzYzMzE4MDkiLCJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9VU0VSIn0seyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpYXQiOjE2NjQwOTc2NTcsImV4cCI6Mzc2NjQwOTc2NTd9.ORHHPOHwcEerLfi4_DGhPvZOwWTyNDvcXuxjR01OLyrlOl_lJldx97VY5UjZ1ntiR9A4Hi8r9J8PBvEKtpkUgg',
-	'Content-Type': f'multipart/form-data; boundary={boundary}'
-}
-
-print(data)
-
-response = requests.post('http://drogrammer.uz/api/v1/category', data = data, headers = headers)
-print(response.text)
+print(payload)
+print(data.decode("utf-8"))
--- main/swagger.py
+++ main/swagger.py
@@ -58,7 +58,7 @@ class Swagger:
 
 	@staticmethod
 	def createProduct(token, raw):
-		pass
+		boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
 
 
 	@staticmethod
@@ -66,5 +66,5 @@ class Swagger:
 		boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
 		data = '{\n    "categoryName": {\n      "uz": {\n        "lang": "uz",\n        "text": '+f'"{raw[0]}"'+'\n},\n      "ru": {\n        "lang": "ru",\n        "text": '+f'"{raw[1]}"'+'\n      },\n      "eng": {\n        "lang": "eng",\n        "text": '+f'"{raw[2]}"'+'\n      }\n    }\n  }'
 		data = f"""--{boundary}\r\nContent-Disposition: form-data; name=categoryCreateRequest;\r\nContent-Type: application/json\r\n\r\n{data}\r\n--{boundary}--\r\n"""
-
+		print(data)
 		response = requests.post(ENDPOINT+'category', data = data.encode(), headers = {'Authorization': token, 'Content-Type': f'multipart/form-data; boundary={boundary}'})
